279a5e096e7451ced0bfd88bbe4bbced5585a1a8
jbjeong <jb.jeong07@gmail.com>
2019-04-09 15:44:52 +0900

    crontab

--- cpu_log.py
+++ cpu_log.py
@@ -4,6 +4,9 @@ from subprocess import call, check_output
 """
 First you need to install lm-sensors
 
+Add to crontab
+*/5 * * * * python cpu_log.py >> cpu.log
+
 """
 
 #result = call('sensors', shell=True)
